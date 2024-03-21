42abca0a2119094f8692b0d7be636b511b8d52fe
Joselu <joseluissgz97@gmail.com>
2024-03-21 13:29:13 +0100

    load functions on document 'load'

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,28 @@
 'use strict';
+
 const navMenu = document.querySelector('.nav');
 const navToggle = document.querySelector('.header__toggle');
 const navClose = document.querySelector('.nav__close');
 
 const navLink = document.querySelectorAll('.nav__link ');
 
-navLink.forEach((el) => {
-  el.addEventListener('click', () => {
-    navLink.forEach((e) => {
-      if (e.classList.contains('nav__link--focus')) {
-        e.classList.remove('nav__link--focus');
-      }
+window.addEventListener('load', () => {
+  navLink.forEach((el) => {
+    el.addEventListener('click', () => {
+      navLink.forEach((e) => {
+        if (e.classList.contains('nav__link--focus')) {
+          e.classList.remove('nav__link--focus');
+        }
+      });
+      el.classList.add('nav__link--focus');
     });
-    el.classList.add('nav__link--focus');
   });
-});
 
-navToggle.addEventListener('click', () => {
-  navMenu.classList.add('nav--show');
-});
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('nav--show');
+  });
 
-navClose.addEventListener('click', () => {
-  navMenu.classList.remove('nav--show');
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('nav--show');
+  });
 });
