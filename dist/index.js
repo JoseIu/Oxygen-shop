eb7225a5ba5e185a6546f9ea56a3e02174ccb253
Joselu <joseluissgz97@gmail.com>
2024-03-21 11:26:24 +0100

    add clas when link is focus

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,19 @@ const navMenu = document.querySelector('.nav');
 const navToggle = document.querySelector('.header__toggle');
 const navClose = document.querySelector('.nav__close');
 
+const navLink = document.querySelectorAll('.nav__link ');
+
+navLink.forEach((el) => {
+  el.addEventListener('click', () => {
+    navLink.forEach((e) => {
+      if (e.classList.contains('nav__link--focus')) {
+        e.classList.remove('nav__link--focus');
+      }
+    });
+    el.classList.add('nav__link--focus');
+  });
+});
+
 navToggle.addEventListener('click', () => {
   navMenu.classList.add('nav--show');
 });
