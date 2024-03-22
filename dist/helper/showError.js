69f09ff7c21b690f7f1938b6c30ffdbca8b88d83
Joselu <joseluissgz97@gmail.com>
2024-03-22 09:59:55 +0100

    showError

---
+++ dist/helper/showError.js
@@ -0,0 +1,13 @@
+const showError = (error, reference) => {
+  const existMessageError = reference.parentElement.querySelector('.message');
+  if (existMessageError) existMessageError.remove();
+
+  const errorMessage = document.createElement('SPAN');
+  errorMessage.textContent = error;
+  errorMessage.classList.add('message');
+
+  reference.classList.add('input--error');
+  reference.parentElement.appendChild(errorMessage);
+};
+
+export default showError;
