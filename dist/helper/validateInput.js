c5e7e979540993fd79bb7f689449052ac9ba83c3
Joselu <joseluissgz97@gmail.com>
2024-03-25 09:24:16 +0100

    refactor_refactoring validaeInput

--- dist/helper/validateInput.js
+++ dist/helper/validateInput.js
@@ -1,21 +1,36 @@
 import removeErrorMessage from './removeErrorMessage.js';
 import showError from './showError.js';
+
 const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const validateInput = (e) => {
-  console.log(e.target.id);
-  if (e.target.value.trim() === '') {
-    showError('The camp is required', e.target);
-    return;
-  }
-  if (e.target.id === 'email') {
-    if (!emailRegex.test(e.target.value)) return showError('email invalid', e.target);
-  }
-  if (e.target.id === 'conditions') {
+const validateInput = (formData, e) => {
+  const { name, email, conditions } = formData;
+
+  console.log(e.target);
+
+  if (e.target.value === '') return showError('camp required', e.target);
+
+  if (e.target.name == 'name') {
+    if (name.length < 2 || name.length > 100)
+      return showError('Name between 2 or 100 letters', e.target);
   }
 
+  if (e.target.name === 'email') {
+    if (!emailRegex.test(email)) return showError('email invalid', e.target);
+  }
+  if (e.target.name === 'conditions') {
+    removeErrorMessage(e.target.parentElement);
+    if (!conditions) return showError('', e.target.parentElement);
+  }
   removeErrorMessage(e.target);
+
+  // CHECK IF WE HAVE DATA
+  if (!name || !email || !conditions) return;
+
+  console.log(formData);
+
+  // submitForm()
 };
 
 export default validateInput;
