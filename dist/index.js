6c540806bae5ec41156d21449170669132e9b861
Joselu <joseluissgz97@gmail.com>
2024-03-25 17:43:55 +0100

    add: function for silder

--- dist/index.js
+++ dist/index.js
@@ -15,17 +15,56 @@ const myForm = document.querySelector('.contact__form');
 const myFormInputs = myForm.querySelectorAll('.form__input');
 
 const container = document.querySelector('.carrousel__container');
+const images = document.querySelectorAll('.carrousel__item');
+
 const points = document.querySelectorAll('.carrousel__point');
 
-console.log(points);
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
 
 const carrusel = () => {
+  let position = 0;
+  const totalPoints = images.length - 1;
+
+  next.addEventListener('click', () => {
+    if (position === totalPoints) return;
+    position++;
+
+    let percent = position * -(100 / 3);
+
+    container.style.transform = `translateX(${percent}%)`;
+
+    points.forEach((ecahPoint) => {
+      ecahPoint.classList.remove('carrousel__point--active');
+    });
+
+    points[position].classList.add('carrousel__point--active');
+  });
+  prev.addEventListener('click', () => {
+    if (position === 0) return;
+    position--;
+
+    let percent = position * -(100 / 3);
+
+    container.style.transform = `translateX(${percent}%)`;
+
+    points.forEach((ecahPoint) => {
+      ecahPoint.classList.remove('carrousel__point--active');
+    });
+
+    points[position].classList.add('carrousel__point--active');
+  });
   points.forEach((point, i) => {
     point.addEventListener('click', () => {
-      let position = i;
-      let percent = position * -(100 / 3);
+      position = i;
+      let percentt = position * -(100 / 3);
+      container.style.transform = `translateX(${percentt}%)`;
 
-      container.style.transform = `translateX(${percent}%)`;
+      points.forEach((ecahPoint) => {
+        ecahPoint.classList.remove('carrousel__point--active');
+      });
+
+      point.classList.add('carrousel__point--active');
     });
   });
 };
