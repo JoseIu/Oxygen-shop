f24fba9afae2e6beca147b4ef10e695c74b9d935
Joselu <joseluissgz97@gmail.com>
2024-03-30 19:51:40 +0100

    refactor: refactoring fuctions, form, slider, navFocus

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+import convertPrices from './helper/convertPrices.js';
+import isFormDataEmpty from './helper/isFromEmpy.js';
+import navFocus from './helper/navFocus.js';
+import progressBarPercent from './helper/progressBarPercent.js';
 import sedForm from './helper/sendForm.js';
+import showModal from './helper/showModal.js';
+import slider from './helper/slider.js';
 import validateInput from './helper/validateInput.js';
 
 const progressBar = document.querySelector('.progress');
@@ -14,129 +20,19 @@ const navLink = document.querySelectorAll('.nav__link ');
 const myForm = document.querySelector('.contact__form');
 const myFormInputs = myForm.querySelectorAll('.form__input');
 
-const container = document.querySelector('.slider__container');
-const images = document.querySelectorAll('.slider__item');
-
-const points = document.querySelectorAll('.slider__point');
-
-const prev = document.querySelector('#prev');
-const next = document.querySelector('#next');
-
 const selectPrices = document.querySelector('#select');
-const basic = document.querySelector('#basic');
-const pro = document.querySelector('#pro');
-const premium = document.querySelector('#premium');
-
-const modal = document.querySelector('.modal');
-const modalClose = document.querySelector('.modal__close');
-const modalForm = document.querySelector('.modal__form');
 
 selectPrices.addEventListener('change', (e) => {
-  const BASIC = 0;
-  const PRO = 25;
-  const PREMINUM = 60;
-
-  let basicOp = 0;
-  let proOp = 0;
-  let premiumOp = 0;
-
-  const convertPrince = async (value) => {
-    const URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies';
-    try {
-      const response = await fetch(`${URL}/eur.json`);
-
-      if (!response.ok) return;
-
-      const data = await response.json();
-
-      const { eur, usd, gbp } = data.eur;
-      if (value === 'eur') {
-        // TO EUR
-        basicOp = (BASIC / usd).toFixed(0);
-        proOp = (PRO / usd).toFixed(0);
-        premiumOp = (PREMINUM / usd).toFixed(0);
-        basic.textContent = `€ ${basicOp}`;
-        pro.textContent = `€ ${proOp}`;
-        premium.textContent = `€ ${premiumOp}`;
-        return;
-      }
-      if (value === 'gbp') {
-        // TO EUR
-        basicOp = (BASIC * gbp).toFixed(0);
-        proOp = (PRO * gbp).toFixed(0);
-        premiumOp = (PREMINUM * gbp).toFixed(0);
-        basic.textContent = `£ ${basicOp}`;
-        pro.textContent = `£ ${proOp}`;
-        premium.textContent = `£ ${premiumOp}`;
-        return;
-      }
-      basicOp = BASIC;
-      proOp = PRO;
-      premiumOp = PREMINUM;
-      basic.textContent = `$ ${basicOp}`;
-      pro.textContent = `$ ${proOp}`;
-      premium.textContent = `$ ${premiumOp}`;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  convertPrince(e.target.value);
+  convertPrices(e.target.value);
 });
-
-const slider = () => {
-  let position = 0;
-  const totalPoints = images.length - 1;
-
-  next.addEventListener('click', () => {
-    if (position === totalPoints) return;
-    position++;
-
-    let percent = position * -(100 / 3);
-
-    container.style.transform = `translateX(${percent}%)`;
-
-    points.forEach((ecahPoint) => {
-      ecahPoint.classList.remove('slider__point--active');
-    });
-
-    points[position].classList.add('slider__point--active');
-  });
-  prev.addEventListener('click', () => {
-    if (position === 0) return;
-    position--;
-
-    let percent = position * -(100 / 3);
-
-    container.style.transform = `translateX(${percent}%)`;
-
-    points.forEach((ecahPoint) => {
-      ecahPoint.classList.remove('slider__point--active');
-    });
-
-    points[position].classList.add('slider__point--active');
-  });
-  points.forEach((point, i) => {
-    point.addEventListener('click', () => {
-      position = i;
-      let percentt = position * -(100 / 3);
-      container.style.transform = `translateX(${percentt}%)`;
-
-      points.forEach((ecahPoint) => {
-        ecahPoint.classList.remove('slider__point--active');
-      });
-
-      point.classList.add('slider__point--active');
-    });
-  });
-};
 
 const formData = {
   name: '',
   email: '',
-  conditions: undefined,
+  conditions: false,
 };
 
-const addEvents = () => {
+const addEventsInputContact = () => {
   myFormInputs.forEach((input) => {
     input.addEventListener('change', getInputValues);
   });
@@ -144,7 +40,11 @@ const addEvents = () => {
 myForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  sedForm(formData);
+  if (!isFormDataEmpty(formData)) {
+    sedForm(formData);
+  } else {
+    console.log('El formulario está vacío, no se realiza ninguna acción');
+  }
 });
 const getInputValues = (e) => {
   if (e.target.name === 'conditions') {
@@ -156,56 +56,9 @@ const getInputValues = (e) => {
   validateInput(formData, e);
 };
 
-const addLinkFocus = (nav) => {
-  nav.addEventListener('click', () => {
-    navLink.forEach((e) => {
-      if (e.classList.contains('nav__link--focus')) {
-        e.classList.remove('nav__link--focus');
-      }
-    });
-    nav.classList.add('nav__link--focus');
-  });
-};
-
-const progressBarPercent = () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  const documentFinal = scrollTop + clientHeight >= scrollHeight;
-
-  const currentPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
-
-  return { currentPercent, documentFinal };
-};
-const showModal = () => {
-  modalClose.addEventListener('click', () => {
-    modal.classList.remove('modal--active');
-    localStorage.setItem('modalClosed', 'true');
-  });
-  const { currentPercent } = progressBarPercent();
-
-  const modalClosed = localStorage.getItem('modalClosed');
-  if (modalClosed !== 'true' && Math.floor(currentPercent) === 25) {
-    modal.classList.add('modal--active');
-  }
-
-  document.addEventListener('click', (event) => {
-    if (!modalForm.contains(event.target) && event.target !== modalForm) {
-      modal.classList.remove('modal--active');
-      localStorage.setItem('modalClosed', 'true');
-    }
-  });
-
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      modal.classList.remove('modal--active');
-      localStorage.setItem('modalClosed', 'true');
-    }
-  });
-};
-
 window.addEventListener('load', () => {
   navLink.forEach((el) => {
-    addLinkFocus(el);
+    navFocus(el);
   });
 
   navToggle.addEventListener('click', () => {
@@ -235,6 +88,6 @@ window.addEventListener('load', () => {
 
     buttonToTop.classList.add('top--show');
   });
-  addEvents();
+  addEventsInputContact();
   slider();
 });
